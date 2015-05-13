e2b82f0776b13a30a79f4f20de89d597a15f58c5
Daniel Neis Araujo <danielneis@gmail.com>
2015-05-13 14:39:58 -0300

    should not be default output for messages

--- message_output_newprocessor.php
+++ message_output_newprocessor.php
@@ -65,7 +65,7 @@ class message_output_newprocessor extends message_output {
      * @return int The default settings
      */
     public function get_default_messaging_settings() {
-        return MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF;
+        return MESSAGE_PERMITTED;
     }
 
     /**
